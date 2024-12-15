a1b6690ea605036005b976f5d115ceed0280c145
ivtavares <igorvtavares@gmail.com>
2024-12-15 12:15:47 -0300

    Adicionar script do merge sort

--- conjuntos/solucoes.py
+++ conjuntos/solucoes.py
@@ -1,5 +1,35 @@
 from typing import List
 
 
-def merge_sort(arr: List[int]):
-    pass
+class Conjunto1:
+    @staticmethod
+    def merge_sort(arr: List[int]) -> List[int]:
+        if len(arr) > 1:
+            half = len(arr) // 2
+            arr_b = arr[:half].copy()
+            arr_c = arr[half:].copy()
+            arr_b = Conjunto1.merge_sort(arr_b)
+            arr_c = Conjunto1.merge_sort(arr_c)
+            return Conjunto1.merge(arr_b, arr_c, arr)
+        return arr
+
+    @staticmethod
+    def merge(arr_b: List[int], arr_c: List[int], arr_a: List[int]) -> List[int]:
+        i = j = k = 0
+        p = len(arr_b)
+        q = len(arr_c)
+
+        while i < p and j < q:
+            if arr_b[i] <= arr_c[j]:
+                arr_a[k] = arr_b[i]
+                i += 1
+            else:
+                arr_a[k] = arr_c[j]
+                j += 1
+            k += 1
+        if i == p:
+            arr_a.append(arr_c[j:])
+        else:
+            arr_a.append(arr_b[i:])
+
+        return arr_a
--- tests/test_solucoes.py
+++ tests/test_solucoes.py
@@ -10,6 +10,6 @@ unsorted_and_sorted_lists = [
 ]
 
 
-@pytest.mark.parametrize("array,sorted_array", unsorted_and_sorted_lists)
-def test_conjunto_1_merge_sort(array, sorted_array):
-    assert s.merge_sort(array) == sorted_array
+@pytest.mark.parametrize("unsorted_list,sorted_list", unsorted_and_sorted_lists)
+def test_conjunto_1_merge_sort(unsorted_list, sorted_list):
+    assert s.Conjunto1.merge_sort(unsorted_list) == sorted_list
