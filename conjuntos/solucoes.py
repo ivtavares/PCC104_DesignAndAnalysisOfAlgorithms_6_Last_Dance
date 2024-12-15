431cc0af61428e93ab3a01ae6857568b7f3b010d
ivtavares <igorvtavares@gmail.com>
2024-12-15 19:41:43 -0300

    adicionar conjunto 2 exercicío 1

--- conjuntos/solucoes.py
+++ conjuntos/solucoes.py
@@ -30,3 +30,26 @@ class Conjunto1:
         if i == p:
             return arr_a[:k] + arr_c[j:]
         return arr_a[:k] + arr_b[i:]
+
+
+class Conjunto2:
+    @staticmethod
+    def knapsack(
+        weight: List[int], value: List[int|float], capacity: int
+    ) -> int | float:
+        size = len(weight)
+        matrix = [[0] * (capacity + 1) for _ in range(size + 1)]
+
+        for i in range(1, size + 1):
+            for j in range(1, capacity + 1):
+                if j - weight[i - 1] >= 0:
+                    matrix[i][j] = max(
+                        matrix[i - 1][j],
+                        value[i - 1] + matrix[i - 1][j - weight[i - 1]],
+                    )
+                else:
+                    matrix[i][j] = matrix[i - 1][j]
+        return matrix[-1][-1]
+    
+    @staticmethod
+    def memory_knapsack
--- tests/test_solucoes.py
+++ tests/test_solucoes.py
@@ -14,3 +14,18 @@ unsorted_and_sorted_lists = [
 @pytest.mark.parametrize("unsorted_list,sorted_list", unsorted_and_sorted_lists)
 def test_conjunto_1_merge_sort(unsorted_list, sorted_list):
     assert s.Conjunto1.merge_sort(unsorted_list) == sorted_list
+
+
+weight_value_capacity_and_optimal_solution = [
+    ([2, 1, 3, 2], [12, 10, 20, 15], 5, 37),  # Solução ótima: itens 1, 2 e 4
+    ([1, 2, 3], [10, 15, 40], 6, 65),  # Solução ótima: todos os itens
+    ([4, 5, 6], [10, 20, 30], 3, 0),  # Solução ótima: nenhum item
+    ([2, 3, 4, 5], [3, 4, 5, 6], 5, 7),  # Solução ótima: itens 1 e 2
+]
+
+
+@pytest.mark.parametrize(
+    "weight,value,capacity,optimal_solution", weight_value_capacity_and_optimal_solution
+)
+def test_conjunto_2_knapsack(weight, value, capacity, optimal_solution):
+    assert s.Conjunto2.knapsack(weight, value, capacity) == optimal_solution
