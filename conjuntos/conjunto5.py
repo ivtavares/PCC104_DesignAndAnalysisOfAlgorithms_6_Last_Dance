717f5b1f3506a098e9890904a4162bf1c9dedcae
ivtavares <igorvtavares@gmail.com>
2024-12-18 01:28:07 -0300

    Adicionar conjunto 5

--- conjuntos/conjunto5.py
+++ conjuntos/conjunto5.py
@@ -1,7 +1,31 @@
 from typing import List
+from collections import deque
 
 
 def branch_and_bound(
     weights: List[int], values: List[int | float], capacity: int
 ) -> int | float:
-    return 0
+    items = [
+        {"w": weight, "v": values[i], "v/w": values[i] / weight}
+        for i, weight in enumerate(weights)
+    ]
+    items = deque(sorted(items, key=lambda payoff: payoff["v/w"], reverse=True))
+    w = 0
+    v = 0
+    W = capacity
+    while items:
+        item = items.popleft()
+        if w + item["w"] <= W:
+            if items:
+                v_w = items[0]["v/w"]
+                in_ub = v + item["v"] + ((W - (item["w"] + w)) * v_w)
+                out_ub = v + ((W - w) * v_w)
+
+                if in_ub > out_ub:
+                    v += item["v"]
+                    w += item["w"]
+            else:
+                v += item["v"]
+                w += item["w"]
+
+    return v
--- tests/test_conjunto5.py
+++ tests/test_conjunto5.py
@@ -4,6 +4,7 @@ from conjuntos.conjunto5 import branch_and_bound
 
 
 weight_value_capacity_and_optimal_solution = [
+    ([4, 7, 5, 3], [40, 42, 25, 12], 10, 65), # Exemplo do livro
     ([2, 1, 3, 2], [12, 10, 20, 15], 5, 37),  # Solução ótima: itens 1, 2 e 4
     ([1, 2, 3], [10, 15, 40], 6, 65),  # Solução ótima: todos os itens
     ([4, 5, 6], [10, 20, 30], 3, 0),  # Solução ótima: nenhum item
